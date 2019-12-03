363b481a2dd676961540852924e07933d40cd8b3
Adam Drew <adamrdrew@live.com>
2019-12-03 08:58:11 -0500

    Add .bin dir and setup script

---
+++ .bin/setup.js
@@ -0,0 +1,117 @@
+/*
+This is the Fenestron Template new project init script.
+This script is intended for single use. If you stumbled upon it in
+your repo you are welcome to delete it :)
+*/
+
+const fs = require('fs')
+const readline = require('readline')
+const exec = require('child_process').exec
+
+// I originally used replace-in-file but then I realized I shouldn't
+// use any dependencies for this, so I wrote this method to respond
+// to the sync method
+class Replace {
+  sync (opts) {
+    var contents = fs.readFileSync(opts.files, (err) => {
+      if (err) throw err
+    })
+    if (contents.indexOf(opts.from) === -1) return
+    const res = contents.replace(opts.from, opts.to)
+    fs.writeFileSync(opts.files, res, (err) => {
+      if (err) throw err
+    })
+  }
+}
+
+// Same story as above. I originaly included shelljs but realized I
+// shouldn't use deps, so this responds to the same method
+class Shell {
+  exec (cmd) {
+    return exec(cmd, (err, stdout, stderr) => {
+      if (err) throw err
+    })
+  }
+}
+
+class Tasks {
+  deleteGit () {
+    fs.rmdir('.git', (err) => {
+      if (err) throw err
+      console.log('.git deleted')
+    })
+  }
+
+  deleteReadMe () {
+    fs.rm('README.md', (err) => {
+      if (err) throw err
+      console.log('README deleted')
+    })
+  }
+
+  deleteLicense () {
+    fs.rm('LICENSE', (err) => {
+      if (err) throw err
+      console.log('LICENESE deleted')
+    })
+  }
+
+  changeAppName () {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+    rl.question('What do you want to call your app?', (answer) => {
+      const replace = new Replace()
+      replace.sync({
+        files: 'package.json',
+        from: 'appnamebeforesetup',
+        to: answer
+      })
+    })
+  }
+
+  safetyCheck () {
+    const shibboleth = 'git@github.com:adamrdrew/fenestron-template.git'
+    const gitConfig = fs.readFileSync('.git/config').toString()
+    if (gitConfig.indexOf(shibboleth) === -1) {
+      throw new Error("Git config doesn't look like the Fenestron Template. Not running to avoid doing damage.")
+    }
+    fs.rmdirSync('.git', {recursive: true})
+    console.log('template git repo deleted')
+  }
+
+  initGit () {
+    const shell = new Shell()
+    shell.exec('git init ; git add -A ; git commit -am "Fenestron Template: Init"')
+    console.log('git initialized')
+  }
+
+  installDeps () {
+    const shell = new Shell()
+    shell.exec('npm install')
+    console.log('deps installed')
+  }
+
+  deleteMyself () {
+    fs.rmdirSync('.bin', (err) => {
+      if (err) throw err
+    })
+  }
+
+}
+
+const tasks = new Tasks()
+try {
+  tasks.safetyCheck()
+  tasks.changeAppName()
+  tasks.deleteGit()
+  tasks.deleteLicense()
+  tasks.deleteReadMe()
+  tasks.deleteMyself()
+  tasks.initGit()
+  tasks.installDeps()
+} catch (err) {
+  console.log(`Something went wrong:\n${err}`)
+  process.exit(1)
+}
