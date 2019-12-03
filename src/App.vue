73cbeaf048e3e309b55462f63318af5483bc4e58
Adam Drew <adamrdrew@live.com>
2019-12-03 18:30:37 -0500

    Fix paneOpen for example PaneButton

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   TitleBar(:back-button="true", title="Title Bar Example")
   SplitView(display-mode="inline", :title-bar-offset="true")
     template(v-slot:pane="slotProps")
-      PaneButton(navigate="home", icon="Home", title="Home")
+      PaneButton(navigate="home", icon="Home", title="Home", :paneOpen="slotProps.paneOpen")
     template(v-slot:content)
       transition(name="page-slide-up")
         router-view
