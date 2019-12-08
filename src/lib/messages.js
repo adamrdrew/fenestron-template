e4f66841c55cd483f4e9b7c29c1ea3a604c47303
Adam Drew <adamrdrew@live.com>
2019-12-07 22:11:47 -0500

    Added an example MessageSender / MessageResponder pair

--- src/lib/messages.js
+++ src/lib/messages.js
@@ -1,11 +1,11 @@
 import { MessageResponder } from 'electron-ipc-helper'
 
-export default function initMessages() {
+export default function initMessages () {
   const exampleMessage = new MessageResponder('exampleMessage')
   exampleMessage.respond(() => {
     return new Promise((resolve, reject) => {
-      window.setTimeout(() =>{
-        resolve('Hello from IPC')
+      setTimeout(() =>{
+        resolve('Hello from IPC!')
       }, 1000)
     })
   })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,31 @@ StackPanel
   .content
     h1 Hello World!
     p Hello from Fenestron UI
+    p {{message}}
+    p(v-if="loading") Waiting on IPC...
+    button(@click="tryIPC", v-else) Try IPC
 </template>
 
 <script>
+import { MessageSender } from 'electron-ipc-helper'
 export default {
   name: 'home',
+  data: () => ({
+    message: "What's IPC say?",
+    loading: false
+  }),
   components: {
+  },
+  methods: {
+    tryIPC () {
+      this.loading = true
+      this.message = ''
+      const sender = new MessageSender('exampleMessage')
+      sender.send().then((message) => {
+        this.loading = false
+        this.message = message
+      })
+    }
   }
 
 }
